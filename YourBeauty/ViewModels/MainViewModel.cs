a06e0f26d9785ee36f8fff06ce1c2e01020547af
Jacob <sencio.jakub@gmail.com>
2024-04-25 23:14:13 +0200

    mvvm 1.x

--- YourBeauty/ViewModels/MainViewModel.cs
+++ YourBeauty/ViewModels/MainViewModel.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using YourBeauty.Commands;
 
 namespace YourBeauty.ViewModels
 {
-    internal class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            RefreshClientsCommand = new RelayCommand(RefreshClients, CanRefreshClients);
+        }
+
+        private void RefreshClients(object obj)
+        {
+            MessageBox.Show("RefreshClients");
+        }
+
+        private bool CanRefreshClients(object obj)
+        {
+            return true;
+        }
+
+        public ICommand RefreshClientsCommand {  get; set; }
     }
 }
