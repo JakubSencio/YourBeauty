f2c830e3cf1f7ceb5008a8e326d1f3dc773cae61
Jacob <sencio.jakub@gmail.com>
2024-04-26 20:08:43 +0200

    update logic

---
+++ YourBeauty/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourBeauty.Models
+{
+    public class Client // Id FirstName LastName Data Zabieg Price Time
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Data {  get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Time {  get; set; }
+        public Group Group { get; set; }
+    }
+}
--- YourBeauty/ViewModels/MainViewModel.cs
+++ YourBeauty/ViewModels/MainViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using YourBeauty.Commands;
+using YourBeauty.Models;
 
 namespace YourBeauty.ViewModels
 {
@@ -13,19 +15,58 @@ namespace YourBeauty.ViewModels
     {
         public MainViewModel()
         {
-            RefreshClientsCommand = new RelayCommand(RefreshClients, CanRefreshClients);
+            RefreshClientsCommand = new RelayCommand(RefreshClients);
+
+            Clients = new ObservableCollection<Client> 
+            { 
+                new Client 
+                { 
+                    FirstName = "Jakub", 
+                    LastName = "Sencio", 
+                    Group = new Group { Id = 1 } 
+                },
+                new Client
+                {
+                    FirstName = "Michał",
+                    LastName = "Nowak",
+                    Group = new Group { Id = 2 }
+                },
+                new Client
+                {
+                    FirstName = "Janusz",
+                    LastName = "Kowalski",
+                    Group = new Group { Id = 3 }
+                },
+            };
+        }
+        public ICommand RefreshClientsCommand { get; set; }
+
+        private Client _selectedClient;
+        public Client SelectedClient
+        {
+            get { return _selectedClient; }
+            set
+            {
+                _selectedClient = value;
+                OnPropertyChanged();
+            }
+        }
+        private ObservableCollection<Client> _clients;
+        public ObservableCollection<Client> Clients
+        {
+            get { return _clients; }
+            set
+            {
+                _clients = value;
+                OnPropertyChanged();
+            }
         }
 
         private void RefreshClients(object obj)
         {
-            MessageBox.Show("RefreshClients");
         }
 
-        private bool CanRefreshClients(object obj)
-        {
-            return true;
-        }
 
-        public ICommand RefreshClientsCommand {  get; set; }
+
     }
 }
